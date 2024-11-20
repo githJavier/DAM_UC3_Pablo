bdaf8ae7b6a3bf935ad3d4546addcc3eaac06ce8
Pedro Javier Pablo Pascual <pedrojavierpablopascual03@gmail.com>
2024-11-20 17:27:43 -0500

    Ultimos detalles

--- app/src/main/java/com/example/dam_uc3_pablo/MainActivity.java
+++ app/src/main/java/com/example/dam_uc3_pablo/MainActivity.java
@@ -1,6 +1,8 @@
     package com.example.dam_uc3_pablo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Button btnbuscarvuelos = findViewById(R.id.boton_buscarvuelos);
+        btnbuscarvuelos.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
+
     }
 }
